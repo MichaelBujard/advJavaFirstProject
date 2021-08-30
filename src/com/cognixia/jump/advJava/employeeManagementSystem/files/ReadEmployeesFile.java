0b99c2f0efb4cf93fe9790e74728e2f33db6fede
MichaelBujard <mbujard@udallas.edu>
2021-08-30 14:21:27 -0500

    Message Commit Message

--- src/com/cognixia/jump/advJava/employeeManagementSystem/files/ReadEmployeesFile.java
+++ src/com/cognixia/jump/advJava/employeeManagementSystem/files/ReadEmployeesFile.java
@@ -19,37 +19,8 @@ import com.cognixia.jump.advJava.employeeManagementSystem.files.InvalidDepartmen
 public class ReadEmployeesFile {
 	
 	
-	/* employees list reduces file data as a list of employees */
-	public static List<Employee> employees = new ArrayList<Employee>();
 
 	public static void main(String[] args) {
-
-		File file = new File("companyResources/employees.txt");
-		FileReader fileReader = null;
-		BufferedReader reader = null;
-		
-		try {
-			
-			fileReader = new FileReader(file);
-			reader = new BufferedReader(fileReader);
-			
-			getEmployeesFromLines(reader);
-			
-		} catch (FileNotFoundException e) {
-			System.out.println("Exception: Could not find file "
-					+ file.getAbsolutePath());
-		} catch (IOException e) {
-			System.out.println("Exception: Could not read!");
-		} finally {
-			
-			try {
-				fileReader.close();
-				reader.close();
-			} catch (IOException e) {
-				System.out.println("Could not close reader objects.");
-			}
-			
-		}
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter one of the following:\n"
@@ -66,6 +37,36 @@ public class ReadEmployeesFile {
 		boolean flag = addFlag || updateFlag || removeFlag || printFlag;
 		
 		while (flag) {
+			/* employees list reduces file data as a list of employees */
+			List<Employee> employees = new ArrayList<Employee>();
+			
+			File file = new File("companyResources/employees.txt");
+			FileReader fileReader = null;
+			BufferedReader reader = null;
+			
+			try {
+				
+				fileReader = new FileReader(file);
+				reader = new BufferedReader(fileReader);
+				
+				getEmployeesFromLines(employees, reader);
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("Exception: Could not find file "
+						+ file.getAbsolutePath());
+			} catch (IOException e) {
+				System.out.println("Exception: Could not read!");
+			} finally {
+				
+				try {
+					fileReader.close();
+					reader.close();
+				} catch (IOException e) {
+					System.out.println("Could not close reader objects.");
+				}
+				
+			}
+			
 			/* ADD */
 			if (addFlag) {
 				addFlag = !addFlag;
@@ -100,13 +101,6 @@ public class ReadEmployeesFile {
 					printWriter.println(newName);
 					printWriter.println(newDepartment.toString());
 					printWriter.println(Integer.toString(newSalary));
-					
-					String n = newName;
-					String d = newDepartment.toString();
-					String s = Integer.toString(newSalary);
-					System.out.println("n = " + n + 
-							", d = " + d +
-							", s = " + s);
 					
 				} catch (IOException e) {
 					System.out.println("'fileWriter' threw 'IOException'");
@@ -156,7 +150,7 @@ public class ReadEmployeesFile {
 				 * If index= then ask for input in range [1, n] where n is the 
 				 *  number of employees in the employee file.
 				 * 
-				 * 3. If invalid input, throw exception. Else, 
+				 * 3. If invalid input, prompt retry. Else, 
 				 * if regex=, search  for string match and select index where 
 				 * name at index i is equal to regex.
 				 * 
@@ -166,88 +160,73 @@ public class ReadEmployeesFile {
 				 * 
 				 * 6. Close file.
 				 */
-				System.out.println("How do you want to select which employee "
-						+ "to update? Enter a number:"
-						+ "\n1. By name match."
-						+ "\n2. By index match.\n\t"
-						+ "i. e. Employees are indexed as list entries "
-						+ "in range [1, n], "
-						+ "\n\twhere n is the index of the last employee.");
-				int option = Integer.valueOf(scan.nextLine());
+				System.out.println("Select which employee "
+						+ "to update by name match.");
 				
-				while (!(option == 1 ^ option == 2)) {
-					System.out.println("Please enter \"1\" or "
-							+ "\"2.\"");
-					option = Integer.valueOf(scan.nextLine());
+				
+				System.out.println("names of all employees:");
+				printEmployeeNames(employees);
+				System.out.println("Enter the name of an employee:");
+				String selectEmployee = scan.nextLine();
+				while (!isValidEmployeeSelection(employees, selectEmployee)) {
+					System.out.println("Please enter a valid employee name.");
+					selectEmployee = scan.nextLine();
 				}
+				System.out.println("Please input name, department, and "
+						+ "salary of " + selectEmployee + " to update, \n"
+						+ "separated by comma, space [\", \"].");
+
+				String params = scan.nextLine();
+				
+				String[] paramsArr = params.split(", ");
+				String newName = paramsArr[0];
+				DepartmentType newDepartment = null;
+				try {
+					newDepartment = getDepartmentType(paramsArr[1]);
+				} catch (InvalidDepartmentException e) {
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				}
+				int newSalary = Integer.parseInt(paramsArr[2]);
+				Employee newEmployee = (newDepartment != null) ? new Employee(newName, newDepartment, newSalary) : null;
+				System.out.println("You put: " + newEmployee.toString());
 				
 				
-				switch (option) {
+				System.out.println("n=" + newName +
+						", d=" + newDepartment.toString() +
+						", s=" + Integer.valueOf(newSalary));
 				
-					case 1:
-						System.out.println("names of all employees:");
-						printEmployeeNames(employees);
-						System.out.println("---------------------------------"
-								+ "----------------------");
-						System.out.println("Enter the name of an employee:");
-						String selectEmployee = scan.nextLine();
-						while (!isValidEmployeeSelection(employees, selectEmployee)) {
-							System.out.println("Please enter a valid employee name.");
-							selectEmployee = scan.nextLine();
-						}
-						System.out.println("Please input name, department, and "
-								+ "salary of " + selectEmployee + " to update, \n"
-								+ "separated by comma, space [\", \"].");
-		
-						String params = scan.nextLine();
-						
-						String[] paramsArr = params.split(", ");
-						String newName = paramsArr[0];
-						DepartmentType newDepartment = null;
-						try {
-							newDepartment = getDepartmentType(paramsArr[1]);
-						} catch (InvalidDepartmentException e) {
-							System.out.println(e.getMessage());
-							e.printStackTrace();
-						}
-						int newSalary = Integer.parseInt(paramsArr[2]);
-						Employee newEmployee = (newDepartment != null) ? new Employee(newName, newDepartment, newSalary) : null;
-						System.out.println("You put: " + newEmployee.toString());
-						
-						
-						System.out.println("n=" + newName +
-								", d=" + newDepartment.toString() +
-								", s=" + Integer.valueOf(newSalary));
-						
-						
-			
-						break;
-					case 2:
-						int employeeIndex = getNumberOfEmployees(employees, file);
-						System.out.println("Please input a number between 1 and " + employeeIndex);
-						int employeeIndexInput = scan.nextInt();
-						while (employeeIndexInput <=0 || employeeIndexInput > employeeIndex) {
-							System.out.println("Please enter a valid value.");
-							employeeIndexInput = scan.nextInt();
-						}
-						break;
-					default:
-						System.out.println("Uh oh. Something went wrong on line 235.");
-						break;
-				
-				}			
+				/* At the first location where the name matches,  override the values of the employee field*/
+				updateEmployeeFromIndex(file, selectEmployee, newName, newDepartment, newSalary);
+	
 					
 			}
 			
 			/* REMOVE */
 			
+			/* Worst-case scenario: User can remove employees by deleting 
+			 * them in the database...But, just do what you did above for update,
+			 * only make a new file with no new value... */
+			
 			if (removeFlag) {
 				removeFlag = !removeFlag;
 				/* allow user to remove employees */
-				System.out.println("Remove the employee! "
-						+ "\nWhich employee "
-						+ "do you want to remove? "
-						+ "\nPlease input their name, department, and salary.");
+				System.out.println("Select which employee "
+						+ "to remove by name match.");
+				System.out.println("names of all employees:");
+				printEmployeeNames(employees);
+				System.out.println("Enter the name of an employee.");
+				System.out.println("\nThe employee with this name will "
+						+ "be removed.");
+				String selectEmployee = scan.nextLine();
+				while (!isValidEmployeeSelection(employees, selectEmployee)) {
+					System.out.println("Please enter a valid employee name.");
+					selectEmployee = scan.nextLine();
+				}
+				
+				/* At the first location where the name matches,  override the values of the employee field*/
+				removeEmployeeFromIndex(file, selectEmployee);
+									
 			}
 			
 			/* PRINT */
@@ -259,7 +238,7 @@ public class ReadEmployeesFile {
 				
 			}
 			
-			System.out.println("Done with command. Please exit or enter "
+			System.out.println("Done with command. Please \"exit\" or enter "
 					+ "another command.");
 			command = scan.nextLine();
 			addFlag = updateFlagBoolean(command, "add");
@@ -267,12 +246,15 @@ public class ReadEmployeesFile {
 			removeFlag = updateFlagBoolean(command, "remove");
 			printFlag = updateFlagBoolean(command, "print");
 			flag = addFlag || updateFlag || removeFlag || printFlag;
+			if (command.toLowerCase().equals("exit")) {
+				System.exit(0);
+			}
 		}
 		
 		
 		
 		
-		
+		scan.close();
 		
 	}
 	
@@ -314,15 +296,16 @@ public class ReadEmployeesFile {
 	public static void printEmployees(List<Employee> list) {
 		Stream<Employee> stream = list.stream();
 		System.out.println("<== Employees list. print method implements Stream: ==>");
-		stream.forEach(result -> System.out.print(result + " "));
-		System.out.println("");
+		stream.forEach(result -> System.out.println(result + " "));
+		System.out.println("-------------------------------------------------------");
 	}
 	
 	public static void printEmployeeNames(List<Employee> list) {
+		System.out.println("call");
 		Stream<Employee> stream = list.stream();
 		System.out.println("<== Employees list. print method implements Stream: ==>");
 		stream.forEach(result -> System.out.println(result.getName()));
-		System.out.println("");
+		System.out.println("-------------------------------------------------------");
 	}
 	
 	public static boolean isValidEmployeeSelection(List<Employee> list, String input) {
@@ -332,7 +315,7 @@ public class ReadEmployeesFile {
 	}
 	
 	
-	public static void getEmployeesFromLines(BufferedReader reader) throws FileNotFoundException, IOException {
+	public static void getEmployeesFromLines(List<Employee> list, BufferedReader reader) throws FileNotFoundException, IOException {
 		
 		String name = null;
 		DepartmentType department = null;
@@ -381,8 +364,8 @@ public class ReadEmployeesFile {
 			employee = (ruler == 1 && department != null) ? new Employee(name, department, salary) : null;
 			
 			/* add employee to list. If null, indicates bad department */
-			if (ruler == 1) {
-				employees.add(employee);
+			if (ruler == 1  && !list.contains(employee)) {
+				list.add(employee);
 			}
 		
 		}
@@ -421,6 +404,279 @@ public class ReadEmployeesFile {
 			e.getStackTrace();
 		}
 		return count / 3;
+	}
+	
+	public static void getEmployeeFromIndex() {
+		System.out.println("'getEmployeeFromIndex'");
+	}
+
+	public static void updateEmployeeFromIndex(File file, String searchName, String name, DepartmentType department, int salary) {
+		
+		String originalName = null;
+		String originalDepartment = null;
+		String originalSalary = null;
+		//create a scanner associated with the file.
+		Scanner sc = null;
+		
+		/* Separate logic for the copying of file and separation into 
+		 * prefix,  postfix, oldLines */
+		int prefixCounter = 0;
+		
+		
+		try {
+			sc = new Scanner(file);
+			String line = sc.nextLine();
+			prefixCounter++;
+			
+			//read each line and 
+			//count the number of lines.
+			// As you do this, set the String values of prefix, 
+			//postfix.
+			while (sc.hasNextLine()) {
+				prefixCounter++;
+				if (line.equals(searchName)) {
+					prefixCounter++;
+					originalName = line;
+					originalDepartment = sc.nextLine();
+					originalSalary = sc.nextLine();
+					break;
+				}
+				line = sc.nextLine();
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		
+		//test and see if the prefix and postfix are set to the right 
+		//values.
+		String prefix = "";
+		String postfix = "";
+		FileReader fileReader = null;
+		BufferedReader reader = null;
+		try {
+			
+			fileReader = new FileReader(file);
+			reader = new BufferedReader(fileReader);
+			
+			String line;
+			int i = 0;
+			while ( (line = reader.readLine()) != null) {
+				if (i < prefixCounter-3) {
+					prefix += line + "\n";
+				}
+				else if (i >= prefixCounter) {
+					postfix += line + "\n";
+				}
+				i++;
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Exception: Could not find file "
+					+ file.getAbsolutePath());
+		} catch (IOException e) {
+			System.out.println("Exception: Could not read!");
+		} finally {
+			
+			try {
+				fileReader.close();
+				reader.close();
+			} catch (IOException e) {
+				System.out.println("Could not close reader objects.");
+			}
+			
+
+		}
+		
+		/* We now have prefix and postfix and fields to input.
+		 * Prefix is a String object, as are the fields and postfix. */
+		/*
+		 * So, overwrite the file with the new information. 
+		 *
+		 * prefix
+		 * name
+		 * department
+		 * salary
+		 * postfix
+		 *
+		 */
+		
+		String wholeFile = prefix + name 
+				+ "\n" + department
+				+ "\n" + salary 
+				+ "\n" + postfix;
+
+		FileWriter fileWriter = null;
+		BufferedWriter buffWriter = null;
+		PrintWriter printWriter = null;
+		
+		try {
+			
+			fileWriter = new FileWriter(file);
+			buffWriter = new BufferedWriter(fileWriter);
+			printWriter = new PrintWriter(buffWriter);
+			printWriter.print(wholeFile);
+		
+		} catch (IOException e) {
+			System.out.println("Exception: 'IOException caught.");
+			e.printStackTrace();
+		} finally {
+			
+			if ( buffWriter != null ) {
+				try {
+					buffWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if ( fileWriter != null ) {
+				try {
+					fileWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				if ( printWriter != null ) {
+					printWriter.close();
+				}
+			}
+			
+		}
+		
+		
+	}
+	
+public static void removeEmployeeFromIndex(File file, String searchName) {
+
+		//create a scanner associated with the file.
+		Scanner sc = null;
+		
+		/* Separate logic for the copying of file and separation into 
+		 * prefix,  postfix, oldLines */
+		int prefixCounter = 0;
+		
+		
+		try {
+			sc = new Scanner(file);
+			String line = sc.nextLine();
+			prefixCounter++;
+			
+			//read each line and 
+			//count the number of lines.
+			// As you do this, set the String values of prefix, 
+			//postfix.
+			while (sc.hasNextLine()) {
+				prefixCounter++;
+				if (line.equals(searchName)) {
+					prefixCounter++;
+					break;
+				}
+				line = sc.nextLine();
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		
+		//test and see if the prefix and postfix are set to the right 
+		//values.
+		String prefix = "";
+		String postfix = "";
+		FileReader fileReader = null;
+		BufferedReader reader = null;
+		try {
+			
+			fileReader = new FileReader(file);
+			reader = new BufferedReader(fileReader);
+			
+			String localLine;
+			int i = 0;
+			while ( (localLine = reader.readLine()) != null) {
+				if (i < prefixCounter-3) {
+					prefix += localLine + "\n";
+				}
+				else if (i >= prefixCounter) {
+					postfix += localLine + "\n";
+				}
+				i++;
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Exception: Could not find file "
+					+ file.getAbsolutePath());
+		} catch (IOException e) {
+			System.out.println("Exception: Could not read!");
+		} finally {
+			
+			try {
+				fileReader.close();
+				reader.close();
+			} catch (IOException e) {
+				System.out.println("Could not close reader objects.");
+			}
+			
+
+		}
+		
+		/* We now have prefix and postfix and fields to input.
+		 * Prefix is a String object, as are the fields and postfix. */
+		/*
+		 * So, overwrite the file with the new information. 
+		 *
+		 * prefix
+		 * name
+		 * department
+		 * salary
+		 * postfix
+		 *
+		 */
+		
+		String wholeFile = prefix + postfix;
+
+		FileWriter fileWriter = null;
+		BufferedWriter buffWriter = null;
+		PrintWriter printWriter = null;
+		
+		try {
+			
+			fileWriter = new FileWriter(file);
+			buffWriter = new BufferedWriter(fileWriter);
+			printWriter = new PrintWriter(buffWriter);
+			printWriter.print(wholeFile);
+		
+		} catch (IOException e) {
+			System.out.println("Exception: 'IOException caught.");
+			e.printStackTrace();
+		} finally {
+			
+			if ( buffWriter != null ) {
+				try {
+					buffWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if ( fileWriter != null ) {
+				try {
+					fileWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				if ( printWriter != null ) {
+					printWriter.close();
+				}
+			}
+			
+		}
+		
+		
 	}
 
 }
